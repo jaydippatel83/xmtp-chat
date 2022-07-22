59624c1307d56e153143be44882b19f855cdbfd5
karanpujara <karanjpujara@gmail.com>
2022-07-22 19:18:20 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ChatBox from './ChatBox';
+
+function App() {
+  return (
+    <div className="App pt-4"> 
+       <ChatBox/>
+    </div>
+  ); 
+}
+
+export default App;
